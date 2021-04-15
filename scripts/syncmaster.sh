0c2a4d26000808eb3cac224fec695207bde59fa9
Yi Zhang <zhanyi@microsoft.com>
2021-04-15 03:00:15 -0700

    submit branch

--- scripts/syncmaster.sh
+++ scripts/syncmaster.sh
@@ -13,10 +13,9 @@ git remote set-url origin https://mszhanyi:${pytorch_token}@github.com/mszhanyi/
 git push
 
 git checkout -b zhanyi/updatevcver
-git push --set-upstream origin zhanyi/updatevcver
+git branch --set-upstream-to=origin zhanyi/updatever
+git pull
 date +%s > report.txt
 git add -A
 git commit -m "Add changes"
-git config --local user.email "mszhanyi@users.noreply.github.com"
-git config --local user.name "mszhanyi"
-git pull
+git push
