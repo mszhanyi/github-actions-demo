4b35a0c789361854c0d1eebc4a32dc889edfc705
Yi Zhang <zhanyi@microsoft.com>
2021-04-15 22:36:04 -0700

    update installvs1.ps1

--- scripts/syncmaster.sh
+++ scripts/syncmaster.sh
@@ -17,7 +17,7 @@ git branch --set-upstream-to=origin/zhanyi/updatevcver zhanyi/updatevcver
 git pull
 date +%s > report.txt
 git add -A
-python3 updatevcver.py
+python3 ../scripts/updatevcver.py
 git commit -a -m "Add changes"
 git status
 git push --set-upstream origin zhanyi/updatevcver
